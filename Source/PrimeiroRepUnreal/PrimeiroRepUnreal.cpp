bdbd9af97fb8864e9ec2a3e85758bad1f4a848f9
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-04-24 17:08:08 -0300

    Add Coin
    Created Coin BluePrint Class!

---
+++ Source/PrimeiroRepUnreal/PrimeiroRepUnreal.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PrimeiroRepUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PrimeiroRepUnreal, "PrimeiroRepUnreal" );
